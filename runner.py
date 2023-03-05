eda7074b028a792b53c28e0777f8e503afeba689
kripto <ignatov.fedor.evgenivich@gmail.com>
2023-03-05 15:40:30 +0400

    update project

--- runner.py
+++ runner.py
@@ -8,6 +8,11 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/artificial_life")
+def artificial_life():
+    return "this page artificial_life"
+
+
 @app.route("/about")
 def about():
     return "test_site"
