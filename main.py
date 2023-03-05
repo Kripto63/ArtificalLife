4847d85356974a5c0bc040ed04a53ac1c1d5941c
kripto <ignatov.fedor.evgenivich@gmail.com>
2023-03-05 15:25:27 +0400

    add about

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+@app.route("/about")
+def about():
+    return "test_site"
+
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
